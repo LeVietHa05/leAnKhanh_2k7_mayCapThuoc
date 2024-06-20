5c3ed3cff153134c949fe968d350c3f41dbd15a0
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-06-20 15:35:52 +0700

    minor change to use FOR LE AN KHANH

--- socketapi.js
+++ socketapi.js
@@ -15,26 +15,42 @@ const socketapi = {
     io: io
 }
 
+const { get } = require("./routes");
+const user = require('./user.json')
+
 io.on("connection", (socket) => {
     console.log("[INFO] new connection: [" + socket.id + "]");
     socket.on("message", (data) => {
-        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic message`);
+        console.log(`message from ${data.clientID ? data.clientID : 'esp'} via socket id: ${socket.id} on topic message`);
         socket.broadcast.emit("message", data);
     });
 
     socket.on('/esp/measure', (data) => {
-        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic /esp/measure`);
+        console.log(`message from ${data.clientID ? data.clientID : 'esp'} via socket id: ${socket.id} on topic /esp/measure`);
         socket.broadcast.emit('/web/measure', data);
     })
 
+    socket.on("/esp/new-card-found", (data) => {
+        console.log(`message from ${data.clientID ? data.clientID : 'esp'} via socket id: ${socket.id} on topic /esp/new-card-found`);
+        console.log(data);
+        let user = getUser(data.uid);
+        if (user) {
+            let stringToSend = getStringToSend(user);
+            console.log(stringToSend);
+            socket.broadcast.emit("/esp/new-card-found-done", { stringToSend: stringToSend });
+        } else {
+            console.log("User not found");
+        }
+    })
+
     socket.on("/web/control", (data) => {
-        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic /web/control`);
+        console.log(`message from ${data.clientID ? data.clientID : 'esp'} via socket id: ${socket.id} on topic /web/control`);
         console.log(data);
         socket.broadcast.emit("/esp/control", data);
     });
 
     socket.on("esp/other", (data) => {
-        console.log(`message from ${data.clientID} via socket id: ${socket.id} on topic esp/other`);
+        console.log(`message from ${data.clientID ? data.clientID : 'esp'} via socket id: ${socket.id} on topic esp/other`);
         socket.broadcast.emit("web/other", data);
         console.log(data);
     });
@@ -51,4 +67,16 @@ io.on("connection", (socket) => {
     });
 })
 
+
+function getUser(uid) {
+    for (let i = 0; i < user.length; i++) {
+        if (+user[i].uid === +uid) {
+            return user[i];
+        }
+    }
+    return null
+}
+function getStringToSend(user) {
+    return user.numofdrugs.join(',');
+}
 module.exports = socketapi;
